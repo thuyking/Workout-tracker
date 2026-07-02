11cbe8dfff9c8d175a39702241120cf420c2df2c
thuyking <162851590+thuyking@users.noreply.github.com>
2026-07-02 15:41:19 +0700

    change1

--- workout-tracker/backend/dist/config/db.js
+++ workout-tracker/backend/dist/config/db.js
@@ -6,9 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectDatabase = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const connectDatabase = async () => {
-    const mongoUri = process.env.MONGO_URI;
+    if (mongoose_1.default.connection.readyState === 1) {
+        return;
+    }
+    const mongoUri = process.env.MONGO_URI || process.env.MONGODB_URI;
     if (!mongoUri) {
-        throw new Error('MONGO_URI is required in .env');
+        throw new Error('MONGO_URI or MONGODB_URI is required in .env');
     }
     await mongoose_1.default.connect(mongoUri);
     console.log('MongoDB connected');
